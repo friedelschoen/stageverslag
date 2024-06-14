a1ec2b475503e0417053944d0d4c360de599b7a2
Friedel Schön <derfriedmundschoen@gmail.com>
2024-06-14 11:04:20 +0200

    concept-submit

--- backup/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cd `dirname $0` 
-exec pandoc stageverslag.md -s \
-    --defaults=options.yml \
-    $@
--- build.sh
+++ build.sh
@@ -2,10 +2,5 @@
 
 cd `dirname $0` 
 exec pandoc stageverslag.md -s \
-    -t pdf \
-    --template=template.tex \
-    --pdf-engine=lualatex \
-    --bibliography=bibliography.bib \
-    --csl=apa-citation.csl \
-    --citeproc \
+    --defaults=options.yml \
     $@
